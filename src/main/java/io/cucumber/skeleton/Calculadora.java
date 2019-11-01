ac6874de8f51b06e8f199efa969d66b3bb7b5db8
Henrique Pinheiro Rodrigues <hrodrigues@alelo.com.br>
2019-11-01 15:51:09 -0300

    Primeira feature iniciada, Soma. Iniciado as Steps Soma

--- src/main/java/io/cucumber/skeleton/Calculadora.java
+++ src/main/java/io/cucumber/skeleton/Calculadora.java
@@ -2,4 +2,15 @@ package io.cucumber.skeleton;
 
 public class Calculadora {
 
+    String funcao;
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+
+    }
 }
+
--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -7,41 +7,25 @@ import cucumber.api.java.pt.Quando;
 import org.junit.Assert;
 
 public class StepDefinitions {
+    Calculadora calc = new Calculadora();
 
-    private double delta;
-    private Calculadora calculadora = new Calculadora();
 
-    @Dado("que eu esteja com a função (.*) aberta")
-    public void queEuEstejaComAFunçãoAberta(String fucntion) {
-        calculadora.setSelectedFunction(fucntion);
+    @Dado("que eu esteja com a função {string} aberta")
+    public void queEuEstejaComAFunçãoAberta(String Soma1) {
+        calc.setFuncao (soma1);
+
     }
 
-    @Quando("eu informar o numero (.*)")
-    public void euInformarONumero(double n1) {
-        calculadora.setN1(n1);
+    @Quando("eu informar o numero {int}")
+    public void euInformarONumero(int arg0) {
     }
 
-    @E("o numero (.*)")
-    public void oNumero(double n2) {
-        calculadora.setN2(n2);
+    @E("o numero {int}")
+    public void oNumero(int arg0) {
     }
 
-    @Então("o resultado deve ser (.*)")
-    public void oResultadoDeveSer(double result) {
-        Assert.assertEquals(result, calculadora.calculate(), delta);
+    @Então("o resultado deve ser {int}")
+    public void oResultadoDeveSer(int arg0) {
     }
 
-    @Quando("eu informar dois numeros aleatorios")
-    public void euInformarDoisNumerosAleatorios() {
-        calculadora.setN1((Math.random() * ((100 - (-100)) + 1)) + -100);
-        calculadora.setN2((Math.random() * ((100 - (-100)) + 1)) + -100);
-    }
-
-    @Então("o resultado será validado")
-    public void oResultadoSeráValidado() {
-        System.out.println("N1: " + calculadora.getN1());
-        System.out.println("N2: " + calculadora.getN2());
-        System.out.println("Resultado: " + calculadora.calculate());
-//        Assert.assertEquals(calculadora.calculate(), );
-    }
 }
